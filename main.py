1142c0ad1addf1424351b4dc7331c8950204cc05
TolyaHuligan <d.zverkov14@gmail.com>
2024-12-16 22:57:44 +0700

    прописать основные функции, добавить вспомогательные в виде заглушек с pass или return"образец"

--- main.py
+++ main.py
@@ -1,31 +1,60 @@
+import random
+import string
+
+
 def get_text():
-    pass
+    print("\nВведите текст")
+    txt = str(input())
+    return txt
 
 
 def gen_text():
+    print("\nВведите длинну текста")
+    len_txt = int(input())
+    all_symbols = string.ascii_uppercase + " "
+    result = ''.join(random.choice(all_symbols) for _ in range(len_txt))
+    return result
+
+
+def palindrome(text):
+    list_of_words = text.split()
+    answer = []
+    for word in list_of_words:
+        if word == reversed(word):
+            answer.append(word)
+    return answer
+
+
+def result(ans):
+    if ans == []:
+        print("Паллиндромов не обнаруженно")
+    else:
+        print(ans)
+
+
+def text_writed():
     pass
 
 
-def palindrome():
+def text_changed():
     pass
 
-
-def result():
-    pass
 
 
 if __name__ == "__main__":
+    text = ""
+    ans = []
     while True:
         print("Выберете пункт меню\n1. Ввод исходных данных вручную\n2. Ввод исходных данных сгенерированных случайным образом\n3. Выполнение алгоритма по заданию\n4. Вывод резульата\n5. Завершение работы программы")
         x = int(input())
         if x == 1:
-            get_text()
+            text = get_text()
         elif x == 2:
-            gen_text()
+            text = gen_text()
         elif x == 3:
-            palindrome()
+            ans = palindrome(text)
         elif x == 4:
-            result()
+            result(ans)
         elif x == 5:
             exit()
         else:
