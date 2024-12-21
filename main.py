780b4d91e1d45904fbde711d93f847408e37113b
TolyaHuligan <d.zverkov14@gmail.com>
2024-12-21 22:51:40 +0700

    Функциональное программирование

--- main.py
+++ main.py
@@ -1,76 +1,63 @@
 import random
 import string
 
-
 def get_text():
-    global writed
-    writed = True
     print("\nВведите текст")
-    txt = str(input())
-    return txt
-
+    return str(input())
 
 def gen_text():
-    global writed
-    writed = True
-    print("\nВведите длинну текста")
+    print("\nВведите длину текста")
     len_txt = int(input())
     all_symbols = string.ascii_uppercase + " "
-    result = ''.join(random.choice(all_symbols) for _ in range(len_txt))
-    return result
+    return ''.join(random.choice(all_symbols) for _ in range(len_txt))
 
+def is_palindrome(word):
+    return word == word[::-1]
 
-def palindrome(text):
-    global chandeg
-    chandeg = True
+def find_palindromes(text):
     list_of_words = text.split()
-    answer = []
-    for word in list_of_words:
-        if word == reversed(word):
-            answer.append(word)
-    return answer
+    return [word for word in list_of_words if is_palindrome(word)]
 
-
-def result(ans):
-    if not ans:
-        print("Паллиндромов не обнаруженно")
-    else:
-        print(ans)
-
+def display_result(ans):
+    (not ans and print("Палиндромов не обнаружено")) or print(ans)
 
 def text_writed(txt):
-    if txt != "":
-        return True
-    else:
-        return False
+    return txt != ""
 
-
-if __name__ == "__main__":
+def main():
     text = ""
     ans = []
     writed = False
     chandeg = False
+
     while True:
         print(
-            "Выберете пункт меню\n1. Ввод исходных данных вручную\n2. Ввод исходных данных сгенерированных случайным образом\n3. Выполнение алгоритма по заданию\n4. Вывод резульата\n5. Завершение работы программы")
+            "Выберите пункт меню\n1. Ввод исходных данных вручную\n2. Ввод исходных данных сгенерированных случайным образом\n3. Выполнение алгоритма по заданию\n4. Вывод результата\n5. Завершение работы программы")
         x = int(input())
+
         if x == 1:
             text = get_text()
+            writed = text_writed(text)
             chandeg = False
         elif x == 2:
             text = gen_text()
+            writed = text_writed(text)
             chandeg = False
         elif x == 3:
             if writed:
-                ans = palindrome(text)
+                ans = find_palindromes(text)
+                chandeg = True
             else:
                 print("Сначала введите текст")
         elif x == 4:
             if chandeg:
-                result(ans)
+                display_result(ans)
             else:
                 print("Сначала проверьте текст на палиндромы")
         elif x == 5:
-            exit()
+            break
         else:
-            print("\nТакого пункта нету\n")
+            print("\nТакого пункта нет\n")
+
+if __name__ == "__main__":
+    main()
